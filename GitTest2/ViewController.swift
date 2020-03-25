940f6266cece37e054044c6ccd3f36610aef456c
Mike O'Connell <guitarmike1966@gmail.com>
2020-03-25 10:34:45 -0500

    Initial Commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by mikey on 3/25/20.
+//  Copyright © 2020 Sand Creek Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
